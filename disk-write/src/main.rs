ff062c62370aba6d928cb437975e0ce21f8a692b
Michael Lazear <lazear@scripps.edu>
2019-03-18 20:10:36 -0700

    Restructure

--- disk-write/src/main.rs
+++ disk-write/src/main.rs
@@ -1,20 +1,25 @@
-use std::fs::{File};
-use std::io::{self, Read, Write, Seek, SeekFrom, BufWriter};
+use std::fs::File;
+use std::io::{self, BufWriter, Read, Seek, SeekFrom, Write};
+use std::process::Command;
 
 fn create_block(output: &str, blocks: usize) -> io::Result<BufWriter<File>> {
     let mut handle = BufWriter::new(File::create(output)?);
     let buffer = [0u8; 1024];
-    for _ in 0 .. blocks {
+    for _ in 0..blocks {
         handle.write_all(&buffer)?;
     }
     Ok(handle)
 }
 
-fn copy_to_file<W: Write + Seek, R: Read>(output: &mut W, input: &mut R, offset: u64) -> io::Result<usize> {
+fn copy_to_file<W: Write + Seek, R: Read>(
+    output: &mut W,
+    input: &mut R,
+    offset: u64,
+) -> io::Result<usize> {
     output.seek(SeekFrom::Start(offset))?;
     let mut written = 0;
     let mut buffer = [0u8; 1024];
-    loop {        
+    loop {
         let n = match input.read(&mut buffer) {
             Ok(0) => break,
             Ok(n) => n,
@@ -26,15 +31,22 @@ fn copy_to_file<W: Write + Seek, R: Read>(output: &mut W, input: &mut R, offset:
     Ok(written)
 }
 
-
 fn main() -> io::Result<()> {
-    let mut handle = create_block("disk.img", 0x1000)?;
-    let mut bootloader = File::open("bootstrap")?;
-    let mut kernel = File::open("../rust-os/target/target/release/rust-os")?;
+    let mut build = Command::new("cargo")
+        .current_dir("../kernel")
+        .args(&["xbuild", "--target", "target.json", "--release"])
+        .spawn()?;
+    if !build.wait()?.success() {
+        panic!("Error executing cargo xbuild");
+    }
+
+    let mut handle = create_block("../disk.img", 0x1000)?;
+    let mut bootloader = File::open("../bootloader/bootstrap")?;
+    let mut kernel = File::open("../kernel/target/target/release/rust-os")?;
 
     let data = kernel.metadata()?.len() as usize;
 
-    copy_to_file(&mut handle, &mut bootloader,  0x400)?;
+    copy_to_file(&mut handle, &mut bootloader, 0x400)?;
     assert_eq!(data, copy_to_file(&mut handle, &mut kernel, 0x800)?);
 
     Ok(())
